e6e840dbf08abb496845a8809b6631202f1c8d4b
Lemuel Ayala <SE44032@grupo.ypf.com>
2025-04-07 15:07:55 -0300

    Se agregan datos de prueba a los mocks de post y tags

---
+++ src/data/mockTags.ts
@@ -0,0 +1,13 @@
+export const allTags = [
+  'Next.js',
+  'React',
+  'TypeScript',
+  'Tailwind',
+  'CSS',
+  'UI/UX',
+  'Authentication',
+  'Performance',
+  // ... más tags
+];
+
+export type Tag = (typeof allTags)[number];
